d10a55b2421db687afd39a0dbd66d049812c9d06
khachatg <75239345+khachatg@users.noreply.github.com>
2020-11-30 02:01:52 -0500

    Adding homework 2
    Basic math program

---
+++ P1HW2_BasicMath_KhachatryanGabriel.py
@@ -0,0 +1,20 @@
+# Basic Math Operations
+# 11/17/2020
+# CTI-110 P1HW2 - Basic Math
+# Gabriel Khachatryan
+
+print("This exercise will take two numbers from the user and print out their sum and product.")
+
+# Prompt the user to input data
+numberOne = float(input("Enter the first number: "))
+numberTwo = float(input("Enter the second number: "))
+
+# Display the entered numbers
+print("First number:", numberOne)
+print("Secondnumber:", numberTwo)
+
+# Add the inputted numbers and display the result
+print("sum:", numberOne + numberTwo)
+
+# Now multiply them and display the result
+print("product:", numberOne * numberTwo)
