e6e0e60e8bfcff08a41d205da3deae83c60cf022
khachatg <75239345+khachatg@users.noreply.github.com>
2020-11-30 01:51:53 -0500

    Adding homework 1
    Customizable sentence in python

---
+++ P1HW1_MadLib_KhachatryanGabriel.py
@@ -0,0 +1,10 @@
+# This program takes input from user to construct a customized sentence.
+
+# Declaring the variables to store user inputs
+first_name = input()
+generic_location = input()
+whole_number = int(input())
+plural_noun = input()
+
+# Using the stored values to construct the custom sentence
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
